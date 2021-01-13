e714ae623a06f5b3caae2fd3347b3c0ec87c3ad9
lee-seung-su <52225690+lee-seung-su@users.noreply.github.com>
2021-01-14 00:35:10 +0900

    assignment2

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,32 +1,61 @@
-package com.example.test
+package com.example.assignment2
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
-//import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Toast
 import org.w3c.dom.Text
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    lateinit var toastButton:Button
+    lateinit var nextButton:Button
+    lateinit var editText:EditText
+    lateinit var textView:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+        intent = Intent(this, SubActivity::class.java)
+        intent.putExtra("from1",editText.text.toString())
 
-        var myName = "홍길동"
-        var aaa = "바보" + "  "+"멍청이"
-        var myAge:Int
-        myAge = 27
-        myAge = myAge+1
+        toastButton.setOnClickListener(this)
+        nextButton.setOnClickListener(this)
+    }
+    fun init(){
+        toastButton=findViewById(R.id.toast_btn)
+        nextButton=findViewById(R.id.next_btn)
+        editText=findViewById(R.id.edit)
+        textView=findViewById(R.id.text_1)
 
+    }
 
-        Log.d("BasicSyntax", "myName = $myName, myAge = $myAge  $aaa")
-    /*
-        var btnSay : Button = findViewById(R.id.btnSay)
-        var textSay : TextView = findViewById(R.id.textSay)
-        btnSay.setOnClickListener{
-            textSay.setText("Hello Kotlin!!!")
+    override fun onClick(v: View?) {
+        var id =v?.id
+        when(id){
+            R.id.toast_btn -> {
+                var toast :Toast = Toast.makeText(this, editText.text, Toast.LENGTH_SHORT)
+                toast.setGravity(Gravity.CENTER_HORIZONTAL,0,0)
+                toast.show()
+            }
+            R.id.next_btn ->{
+                startActivityForResult(intent,99)
+            }
         }
-      */
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode==Activity.RESULT_OK){
+            textView.text=data?.getStringExtra("return")
+        }
+
     }
 }
